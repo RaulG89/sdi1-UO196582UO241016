9f2d98c119d3d1dc1b1ca85de23ee9bfb50fe6a5
MarcosRV87 <neverstepalone@gmail.com>
2018-02-21 14:17:23 +0100

    First commit. Project architecture based on second laboratory.

---
+++ src/main/java/com/uniovi/Sdi1Uo196582Uo241016Application.java
@@ -0,0 +1,12 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1Uo196582Uo241016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1Uo196582Uo241016Application.class, args);
+	}
+}
---
+++ src/test/java/com/uniovi/Sdi1Uo196582Uo241016ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uniovi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sdi1Uo196582Uo241016ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
