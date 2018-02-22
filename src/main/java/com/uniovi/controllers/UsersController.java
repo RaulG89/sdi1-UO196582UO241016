c92968d2e294874f2a8857ae54f49a5c1e79f179
MarcosRV87 <neverstepalone@gmail.com>
2018-02-22 18:54:09 +0100

    Sign-up done. Internationalization (i18) configuration completed.

--- src/main/java/com/uniovi/controllers/UsersController.java
+++ src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,12 +1,19 @@
 package com.uniovi.controllers;
 
+import org.aspectj.weaver.SignatureUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.uniovi.entities.User;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
@@ -17,6 +24,9 @@ public class UsersController {
 	@Autowired
 	private SecurityService securityService;
 	
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
+	
 	@RequestMapping(value="/login")
 	public String login(Model model) {
 		return "login";
@@ -25,6 +35,23 @@ public class UsersController {
 	@RequestMapping(value="/home")
 	public String home(Model model) {
 		return "home";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String setUser(@ModelAttribute @Validated User user, BindingResult result, Model model) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
+		usersService.addUser(user);
+		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
+		return "redirect:home";
 	}
 
 }
