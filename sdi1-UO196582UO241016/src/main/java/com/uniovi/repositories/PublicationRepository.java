2e386336d4c2a2a09dcdd8fa39e1e8277187b530
Raúl Gómez Pérez <raul.infotek@gmail.com>
2018-03-20 01:58:20 +0100

    Added publication repository and publication service.

---
+++ sdi1-UO196582UO241016/src/main/java/com/uniovi/repositories/PublicationRepository.java
@@ -0,0 +1,14 @@
+package com.uniovi.repositories;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.uniovi.entities.Publication;
+import com.uniovi.entities.User;
+
+public interface PublicationRepository extends CrudRepository<Publication, Long> {
+
+	List<Publication> findAll();
+	
+	List<Publication> findPublicationByUser(User user);
+	
+}
---
+++ sdi1-UO196582UO241016/src/main/java/com/uniovi/services/PublicationsService.java
@@ -0,0 +1,27 @@
+package com.uniovi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uniovi.entities.Publication;
+import com.uniovi.entities.User;
+import com.uniovi.repositories.PublicationRepository;
+
+@Service
+public class PublicationsService {
+	
+	@Autowired
+	private PublicationRepository publicationRepository;
+	
+	public List<Publication> getAllPublications(){
+		return publicationRepository.findAll();
+	}
+	
+	public List<Publication> getPublicationsByUser(User user){
+		return publicationRepository.findPublicationByUser(user);		
+	}
+	
+
+}
